a00712651f69b10a7ff4412fa91f26e09febd303
AlexKatros <alexkatros@gmail.com>
2019-12-14 11:02:52 -0500

    Delete profile/users

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ const connectDB = async () => {
 		await mongoose.connect(db, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
-			useCreateIndex: true
+			useCreateIndex: true,
+			useFindAndModify: false
 		}); //await before a function that returns a promise
 		console.log('MongoDB Connected...');
 	} catch (err) {
